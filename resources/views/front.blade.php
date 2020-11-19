2d48e03803a8320c530da9c4a78bd1998d38d506
barmaley4ik <my.ofice@gmail.com>
2020-11-19 17:55:01 +0200

    facebook pixel

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -10,6 +10,23 @@
     <link href="./img/icon_homex48.png " type="image/png " rel="icon " sizes="48x48 " />
     <link rel="preconnect " href="https://fonts.gstatic.com ">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@400;500;700&display=swap " rel="stylesheet ">
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '401910537915615');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=401910537915615&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 </head>
 
 <body class="font-roboto " >
@@ -186,6 +203,7 @@
     </div>
     </div>
     <script src="{{ mix('js/front.js') }}"></script>
+
 </body>
 
 </html>
